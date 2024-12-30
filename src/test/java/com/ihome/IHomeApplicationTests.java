b5684d959b1b403fef51a3b6f31c1094c3a9449c
Zachary-Doke <168358359+Zachary-Doke@users.noreply.github.com>
2024-12-30 15:33:53 +0800

    创建Springboot项目

---
+++ src/test/java/com/ihome/IHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
